4a33377ba07fd6ac66e082a7b9e3c188a9d7c2ee
segun-ade <80776153+segun-ade@users.noreply.github.com>
2026-03-11 18:07:17 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,7 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -81,6 +82,7 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -100,6 +102,7 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
